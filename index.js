67dc7c74cc526b0203d9483c64a4f0754c9a2365
Skaelv <kevin.antoine@intercom.io>
2017-01-20 11:54:58 +0000

    use ic-retext

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var remark = require('remark');
 var retext = require('retext');
 var control = require('remark-message-control');
 var english = require('retext-english');
-var shopify = require('retext-shopify');
+var intercom = require('ic-retext');
 var usage = require('retext-usage');
 var simplify = require('retext-simplify');
 var equality = require('retext-equality');
@@ -90,7 +90,7 @@ var profanitiesConfig = {
 
 var text = retext()
     .use(english)
-    .use(shopify)
+    .use(intercom)
     .use(usage)
     .use(simplify, simplifyConfig)
     .use(equality, equalityConfig)
@@ -139,7 +139,7 @@ function alex(value, allow) {
         'name': 'alex',
         'disable': allow,
         'source': [
-            'retext-shopify',
+            'ic-retext',
         ]
     }));
 }
